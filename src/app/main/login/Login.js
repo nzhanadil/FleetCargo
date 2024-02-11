fdf745237fd83434d05324417b09f9a447ecfe06
Peri2505 <moldobaevaperi@yahoo.com>
2024-02-10 20:33:29 -0600

    The Firebase logo was replaced with cargoFleet one and FireBase logo was removed.

--- src/app/main/login/Login.js
+++ src/app/main/login/Login.js
@@ -56,7 +56,7 @@ function Login() {
           <CardContent className="flex flex-col items-center justify-center w-full max-w-320">
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.2 } }}>
               <div className="flex items-center mb-48">
-                <img className="logo-icon w-48" src="assets/images/logos/fuse.svg" alt="logo" />
+                <img className="logo-icon w-48" src="assets/images/logos/logo.png" alt="logo" />
                 <div className="border-l-1 mr-4 w-1 h-40" />
                 <div>
                   <Typography className="text-24 font-semibold logo-text" color="inherit">
